5e8564af4cd69ee965712c9ba624a62b8c16acea
Sourabhkhera11 <sourabhkhera11@gmail.com>
2025-11-14 12:09:16 +0530

    navigation bar

--- index.js
+++ index.js
@@ -6,23 +6,31 @@ const next=document.getElementById("next");
 
 let index=0;
 
+const indi=document.querySelectorAll('.indicators div');
+console.log(indi);
+
 const showImage=(i)=>{
     // images.forEach((img)=>{
     //     img.classList.remove('active');
     // })
     if(i==0){
         images[images.length-1].classList.remove('active');
+        indi[indi.length-1].classList.remove('active');
     }
     else{
         images[i-1].classList.remove('active');
+        indi[i-1].classList.remove('active');
     }
     if(i==images.length){
         images[0].classList.remove('active');
+        indi[0].classList.remove('active');
     }
     else{
         images[(i+1)%images.length].classList.remove('active');
+        indi[(i+1)%indi.length].classList.remove('active');
     }
     images[i%images.length].classList.add('active');
+    indi[i%indi.length].classList.add('active');
 }
 
 prev.addEventListener("click",()=>{
@@ -34,9 +42,11 @@ next.addEventListener("click",()=>{
     index=(index+1)%images.length;
     showImage(index);
 })
-const indicator=document.querySelectorAll('.indicators div');
-console.log(indicator);
 
-indicator.addEventListener("click",()=>{
-    
+const ele=document.querySelector(".indicators");
+//event Deligation
+ele.addEventListener("click",(e)=>{
+    const imgNo=e.target.id;
+    console.log(imgNo);
+    showImage(imgNo);
 })
