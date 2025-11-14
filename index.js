335ab319fa573a5fae36a9c50df06355b83e1283
Sourabhkhera11 <sourabhkhera11@gmail.com>
2025-11-14 18:16:53 +0530

    Optimise js using onclick instead of getelmentbyid

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-const images=document.querySelectorAll('.carousel span');
-console.log(images);
-
-const prev=document.getElementById("prev");
-const next=document.getElementById("next");
-
+const images = document.querySelectorAll('.carousel span');
 let index=0;
-
 const indi=document.querySelectorAll('.indicators div');
-console.log(indi);
-
 const showImage=(i)=>{
-    // images.forEach((img)=>{
-    //     img.classList.remove('active');
-    // })
-    if(i==0){
-        images[images.length-1].classList.remove('active');
-        indi[indi.length-1].classList.remove('active');
-    }
-    else{
-        images[i-1].classList.remove('active');
-        indi[i-1].classList.remove('active');
-    }
-    if(i==images.length){
-        images[0].classList.remove('active');
-        indi[0].classList.remove('active');
-    }
-    else{
-        images[(i+1)%images.length].classList.remove('active');
-        indi[(i+1)%indi.length].classList.remove('active');
-    }
+    document.querySelector(".imgBlock.active").classList.remove('active');
+    document.querySelector(".indicator.active").classList.remove('active');
     images[i%images.length].classList.add('active');
     indi[i%indi.length].classList.add('active');
 }
-
-prev.addEventListener("click",()=>{
+function previous(){
     index=(index-1+images.length)%images.length;
-    console.log(index);
     showImage(index);
-})
-
-next.addEventListener("click",()=>{
+}
+function next(){
     index=(index+1)%images.length;
-    console.log(index);
     showImage(index);
-})
+}
+function indicator(event){
+    index=event.target.id; //to main consistency
+    showImage(index);
+}
 
-const ele=document.querySelector(".indicators");
-//event Deligation
-ele.addEventListener("click",(e)=>{
-    const imgNo=e.target.id;
-    console.log(imgNo);
-    index=imgNo;
-    showImage(imgNo);
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(i==0){
+//         images[images.length-1].classList.remove('active');
+//         indi[indi.length-1].classList.remove('active');
+//     }
+//     else{
+//         images[i-1].classList.remove('active');
+//         indi[i-1].classList.remove('active');
+//     }
+//     if(i==images.length){
+//         images[0].classList.remove('active');
+//         indi[0].classList.remove('active');
+//     }
+//     else{
+//         images[(i+1)%images.length].classList.remove('active');
+//         indi[(i+1)%indi.length].classList.remove('active');
+//     }
